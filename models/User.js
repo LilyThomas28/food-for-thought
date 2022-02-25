4ecfbbb0463035898c11dff7cf7787e7e95700dd
Lily Thomas <lilylynettethomas@gmail.com>
2022-02-24 17:23:57 -0800

    Created base User model

--- models/User.js
+++ models/User.js
@@ -1,21 +1,41 @@
-// User:
-// username:
-// String
-// Unique
-// Required
-// Trimmed
+const { Schema, model } = require('mongoose');
 
-// email:
-// String
-// Required
-// Unique
-// Must match a valid email address (look into Mongoose's matching validation)
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            require: true,
+            unique: true,
+            trim: true
+        },
+        email:{
+            type: String,
+            require: true,
+            unique: true,
+            validate: [validateEmail, 'Please fill a valid email address'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    // {
+    //     toJSON: {
+    //         virtuals: true;
+    //     },
+    //     id: false,
+    // }
+);
 
-// thoughts:
-// Array of _id values referencing the Thought model
-
-// friends:
-// Array of _id values referencing the User model (self-reference)
 
 // Schema Settings:
 // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
