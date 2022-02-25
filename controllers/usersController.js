b2aec1b259d539eb6c0728212b8613e59df2daa7
Lily Thomas <lilylynettethomas@gmail.com>
2022-02-24 18:01:03 -0800

    Added two router functions to usercontroller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,9 +4,21 @@ const { User, Thought } = require('../models');
 module.exports = {
     // All route functions
     // GET all users
-
+    getAllUsers(req, res) {
+        User.Find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
     // GET a single user by _id
-
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "No user with that id" })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    }
     // POST new user
 
     // PUT update user by _id
