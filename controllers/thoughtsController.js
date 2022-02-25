6217259afb6877acd41d87ecef144ca0a90a1725
Lily Thomas <lilylynettethomas@gmail.com>
2022-02-24 17:53:47 -0800

    Added sudo code to thought and user controllers

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,1 +1,18 @@
 // export functions for handling thought requests
+const { Thought, User } = require('../models');
+// Add any aggregate functions
+
+module.exports = {
+    // All route functions
+    // GET all thoughts
+
+    // GET single thought by _id
+
+    // POST new thought and push to user's thought array
+
+    // PUT update thought by _id
+
+    // DELETE remove thought by _id
+
+    
+}
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,1 +1,19 @@
 // export functions for handling user requests
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // All route functions
+    // GET all users
+
+    // GET a single user by _id
+
+    // POST new user
+
+    // PUT update user by _id
+
+    // DELETE user by _id
+
+    // POST to add new friend to user's friend list
+
+    // DELETE remove friend from user's friend list
+}
