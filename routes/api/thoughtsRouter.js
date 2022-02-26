e1446354c459c9de4510f8d9d416a6de2073ba61
Lily Thomas <lilylynettethomas@gmail.com>
2022-02-26 00:09:59 -0800

    Add routes in thoughtsController

--- routes/api/thoughtsRouter.js
+++ routes/api/thoughtsRouter.js
@@ -1,15 +1,36 @@
 const router = require('express').Router();
 // add routes for thoughts
+const {
+    getAllThoughts,
+    getSingleThought,
+    createNewThought,
+    updateThought,
+    deleteThought,
+    createNewReaction,
+    deleteReaction,
+} = require('../../controllers/thoughtsController');
 
 // /api/thoughts  :
-// GET to get all thoughts
-// GET to get a single thought by its _id
-// POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
-// PUT to update a thought by its _id
-// DELETE to remove a thought by its _id
+router.route('/')
+    // GET to get all thoughts
+    .get(getAllThoughts)
+    // POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+    .post(createNewThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId')
+    // GET to get a single thought by its _id
+    .get(getSingleThought)
+    // PUT to update a thought by its _id
+    .put(updateThought)
+    // DELETE to remove a thought by its _id
+    .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions  :
-// POST to create a reaction stored in a single thought's reactions array field
-// DELETE to pull and remove a reaction by the reaction's reactionId value
+router.route('/:thoughtId/reactions')
+    // POST to create a reaction stored in a single thought's reactions array field
+    .post(createNewReaction)
+    // DELETE to pull and remove a reaction by the reaction's reactionId value
+    .delete(deleteReaction);
 
 module.exports = router;
