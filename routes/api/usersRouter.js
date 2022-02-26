79e4369c557064e27ef01ae35c9555b7ab049798
Lily Thomas <lilylynettethomas@gmail.com>
2022-02-26 00:13:53 -0800

    Add routes to usersRouter

--- routes/api/usersRouter.js
+++ routes/api/usersRouter.js
@@ -1,15 +1,36 @@
 const router = require('express').Router();
 // add routes for users
-// /api/users  :
-// GET all users
-// GET a single user by its _id and populated thought and friend data
-// POST a new user:
-// PUT to update a user by its _id
-// DELETE to remove user by its _id
-// BONUS: Remove a user's associated thoughts when deleted.
+const {
+    getAllUsers,
+    getSingleUser,
+    createNewUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/usersController');
 
+// /api/users  :
+router.route('/')
+    // GET all users
+    .get(getAllUsers)
+    // POST a new user:
+    .post(createNewUser);
+
+// /api/users/:userId
+router.route('/:userId')
+    // GET a single user by its _id and populated thought and friend data
+    .get(getSingleUser)
+    // PUT to update a user by its _id
+    .put(updateUser)
+    // DELETE to remove user by its _id
+    .delete(deleteUser);
+    
 // /api/users/:userId/friends/:friendId  :
-// POST to add a new friend to a user's friend list
-// DELETE to remove a friend from a user's friend list
+router.route('/:userId/friends/:friendId')
+    // POST to add a new friend to a user's friend list
+    .post(addFriend)
+    // DELETE to remove a friend from a user's friend list
+    .delete(deleteFriend);
 
 module.exports = router;
